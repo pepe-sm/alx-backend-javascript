a42e4992e4572495e8771b5eb722b2c34aa59ec8
Stephen Gitau <stephenpepe.sm@gmail.com>
2024-02-27 10:03:37 -0800

    console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Stephen Gitau <https://github.com/pepe-sm>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
